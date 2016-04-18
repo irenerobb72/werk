494916ff491425f944d6f4eb8f077c52f7d7fc17
Irene Robb <irenerobb72@gmail.com>
2016-04-18 19:26:55 -0400

    initial file setup

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
+import reactMixin from 'react-mixin'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
 
-export class App extends Component {
+export class Products extends Component {
   constructor(props) {
     super(props)
   }
-
   render() {
-    return (
-      <div className="app">
-      </div>
+  return (
+    <div className="products">
+  
+    </div>
     )
   }
 }
