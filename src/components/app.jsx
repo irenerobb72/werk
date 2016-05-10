d33fb15f8bfaa8a4c4ca58c2dcfb7b5e269c30fa
Irene Robb <irenerobb72@gmail.com>
2016-05-10 10:15:39 -0400

    initial

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render() {
+    return(
+      <span>
+        <h2>lotr</h2>
+      </span>
+  )}
+}
+
+export default App
